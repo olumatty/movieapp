ba7c78baed7f20eb7cadb7ac3d730ac12e77bef7
olumatty <olumatty@gmail.com>
2025-05-09 00:33:18 +0100

    movie screens setup

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center px-2">
+      <Text className="text-dark-200 font-bold text-5xl">Welcome!</Text>
+    </View>
+  );
+}
