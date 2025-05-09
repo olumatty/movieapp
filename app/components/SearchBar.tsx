deae71da4ab831e1562893643f41c90f3a34678b
olumatty <olumatty@gmail.com>
2025-05-09 17:08:15 +0100

    Api call

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -2,13 +2,18 @@ import { icons } from '@/constants/icons'
 import React from 'react'
 import { Image, TextInput, View } from 'react-native'
 
-const SearchBar = () => {
+
+interface props {
+  onPress: () => void
+  placeholder: string
+}
+const SearchBar = ({onPress, placeholder}: props) => {
   return (
     <View className='flex-row items-center justify-center bg-dark-200 rounded-full px-5 py-4'>
         <Image source={icons.search} className='size-5' tintColor="#ab8bff" resizeMode='contain'/>
         <TextInput
-          onPress={() => {}}
-          placeholder='Search for movies'
+          onPress={onPress}
+          placeholder={placeholder}
           value=''
           className='text-white flex-1 ml-2'
           onChangeText={() => {}}
---
+++ services/useFetch.ts
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+};
