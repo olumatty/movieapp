17124f75b0cdcabbed1c00df3cccccd598839f17
olumatty <olumatty@gmail.com>
2025-05-09 21:48:11 +0100

    Api call

--- services/useFetch.ts
+++ services/useFetch.ts
@@ -1,7 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
   const [data, setData] = useState<T | null>(null);
+
   const [loading, setLoading] = useState(false);
+
   const [error, setError] = useState<Error | null>(null);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const data = await fetchFunction();
+      setData(data);
+    } catch (err) {
+      setError(err instanceof Error ? err : new Error("Something went wrong"));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const reset = () => {
+    setData(null);
+    setError(null);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
+    }
+  }, []);
+
+  return { data, loading, error, refetch: fetchData, reset };
 };
+
+export default useFetch;
